8926d6924215ccc63672dfc0c0eb8ccb81102c5e
Manyu Lakhotia <lakemonyew@gmail.com>
2024-01-07 18:59:24 -0600

    ^py with dict note

--- py_prac/src/main.py
+++ py_prac/src/main.py
@@ -591,6 +591,7 @@ def dictionary_prac():
     d['b'] = d['b'] + 1
     print(d)
     # unpacking dictionaries to merge. like spread in js
+    # NOTE: keeps last. so 'D': 4 will be in the merged dict
     my_first_dict = {'A': 1, 'B': 2, 'D': 2}
     my_second_dict = {'C': 3, 'D': 4}
     my_merged_dict = {**my_first_dict, **my_second_dict}
