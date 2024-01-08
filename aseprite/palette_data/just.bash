9d4b1df91f4677b132f39dd4a1e405d476cd27ed
Manyu Lakhotia <mlakhotia@conservis.ag>
2024-01-08 13:58:31 -0600

    Allow for multiple color_ramp subs

--- aseprite/palette_data/just.bash
+++ aseprite/palette_data/just.bash
@@ -1,18 +1,30 @@
 function just_get_color_map {
   local color_replace_config;color_replace_config="$(cat ./color_replace_config.json)";
   local color_palette; color_palette="$(echo "$color_replace_config" | jq -r ".color_palette")";
-  local from_color_ramp; from_color_ramp="$(echo "$color_replace_config" | jq -r ".from.color_ramp")";
-  local to_color_ramp; to_color_ramp="$(echo "$color_replace_config" | jq -r ".to.color_ramp")";
+
+  local replacement_length; replacement_length="$(echo "$color_replace_config" | jq -r ".replacements | length")"
   local query;
-  query="
-  Select
-    '{' + f'from=Color({a.r}, {a.g}, {a.b})' + '}' + f', to=Color({b.r}, {b.g}, {b.b})' + '},' as color_mapping
-  join ./colors.csv on a.color_palette = b.color_palette and a.color_ramp_priority = b.color_ramp_priority
-  where
-    a.color_palette == '${color_palette}'
-    and a.color_ramp == '${from_color_ramp}'
-    and b.color_ramp == '${to_color_ramp}'
-  order by a.color_ramp_priority asc
-  ";
-  cat ./colors.csv | rbql --with-header --query "$query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g';
+  local from_color_ramp;
+  local to_color_ramp;
+  local query_result;
+  for ((idx=0; idx<replacement_length; idx++)); do
+    from_color_ramp="$(echo "$color_replace_config" | jq -r ".replacements[$idx].from_color_ramp")";
+    to_color_ramp="$(echo "$color_replace_config" | jq -r ".replacements[$idx].to_color_ramp")";
+    query="
+    Select
+      '{' + f'from=Color({a.r}, {a.g}, {a.b})' + f', to=Color({b.r}, {b.g}, {b.b})' + '},' as color_mapping
+    join ./colors.csv on a.color_palette = b.color_palette and a.color_ramp_priority = b.color_ramp_priority
+    where
+      a.color_palette == '${color_palette}'
+      and a.color_ramp == '${from_color_ramp}'
+      and b.color_ramp == '${to_color_ramp}'
+    order by a.color_ramp_priority asc
+    ";
+    query_result="$(cat ./colors.csv | rbql --with-header --query "$query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
+    if [[ "${idx}" == "0" ]]; then
+      echo "$query_result";
+    else
+      echo "$query_result" | tail -n +2;
+    fi
+  done;
 }
--- aseprite/scripts/change_colors.lua
+++ aseprite/scripts/change_colors.lua
@@ -7,12 +7,19 @@ if not sprite then
 end
 
 local colorPairs = {
-  {from=Color(23, 32, 56)}, to=Color(30, 29, 57)},
-  {from=Color(37, 58, 94)}, to=Color(64, 39, 81)},
-  {from=Color(60, 94, 139)}, to=Color(122, 54, 123)},
-  {from=Color(79, 143, 186)}, to=Color(162, 62, 140)},
-  {from=Color(115, 190, 211)}, to=Color(198, 81, 151)},
-  {from=Color(164, 221, 219)}, to=Color(223, 132, 165)},
+  {from=Color(25, 51, 45), to=Color(52, 28, 39)},
+{from=Color(37, 86, 46), to=Color(96, 44, 44)},
+{from=Color(70, 130, 50), to=Color(136, 75, 43)},
+{from=Color(117, 167, 67), to=Color(190, 119, 43)},
+{from=Color(168, 202, 88), to=Color(222, 158, 65)},
+{from=Color(208, 218, 145), to=Color(232, 193, 112)},
+{from=Color(77, 43, 50), to=Color(36, 21, 39)},
+{from=Color(122, 72, 65), to=Color(65, 29, 49)},
+{from=Color(173, 119, 87), to=Color(117, 36, 56)},
+{from=Color(192, 148, 115), to=Color(165, 48, 48)},
+{from=Color(215, 181, 148), to=Color(207, 87, 60)},
+{from=Color(231, 213, 179), to=Color(218, 134, 62)},
+
 }
 
 for _, pair in ipairs(colorPairs) do
