e5c08bb47369ec13baf575e0eee5f4769b0d580f
Manyu Lakhotia <mlakhotia@conservis.ag>
2024-01-09 13:49:19 -0600

    +just_get_color_idx_change;!just_get_color_ramp_shift_map

--- aseprite/palette_data/just.bash
+++ aseprite/palette_data/just.bash
@@ -35,6 +35,9 @@ function just_get_color_ramp_shift_map {
   local replacement_length; replacement_length="$(echo "$config" | jq -r ".replacements | length")"
   local query;
   local color_ramp;
+  local shift_step;
+  local start_idx;
+  local end_idx;
   local query_result;
   for ((idx=0; idx<replacement_length; idx++)); do
     color_ramp="$(echo "$config" | jq -r ".replacements[$idx].color_ramp")";
@@ -53,6 +56,36 @@ function just_get_color_ramp_shift_map {
       and int(a.color_ramp_idx) <= ${end_idx}
       and int(a.color_ramp_idx) == (int(b.color_ramp_idx) + ${shift_step})
     order by a.color_ramp_idx asc
+    ";
+    query_result="$(cat ./colors.csv | rbql --with-header --query "$query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
+    if [[ "${idx}" == "0" ]]; then
+      echo "$query_result";
+    else
+      echo "$query_result" | tail -n +2;
+    fi
+  done;
+}
+
+function just_get_color_idx_change {
+  local config;config="$(cat ./color_idx_change_config.json)";
+  local color_palette; color_palette="$(echo "$config" | jq -r ".color_palette")";
+  local replacement_length; replacement_length="$(echo "$config" | jq -r ".replacements | length")"
+  local query;
+  local from_idx;
+  local to_idx;
+  local query_result;
+  for ((idx=0; idx<replacement_length; idx++)); do
+    from_idx="$(echo "$config" | jq -r ".replacements[$idx].from_idx")";
+    to_idx="$(echo "$config" | jq -r ".replacements[$idx].to_idx")";
+    query="
+    Select
+      '{' + f'from=Color({a.r}, {a.g}, {a.b})' + f', to=Color({b.r}, {b.g}, {b.b})' + '},' as color_mapping
+      join ./colors.csv on a.color_palette = b.color_palette
+    where
+      a.color_palette == '${color_palette}'
+      and int(a.idx) == ${from_idx}
+      and int(b.idx) == ${to_idx}
+    order by a.idx asc
     ";
     query_result="$(cat ./colors.csv | rbql --with-header --query "$query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
     if [[ "${idx}" == "0" ]]; then
