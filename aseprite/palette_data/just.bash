c7899edf29602ec872e73441870edd17db773e95
Manyu Lakhotia <mlakhotia@conservis.ag>
2024-01-09 16:38:45 -0600

    +just_get_color_ramp_flip

--- aseprite/palette_data/just.bash
+++ aseprite/palette_data/just.bash
@@ -95,3 +95,65 @@ function just_get_color_idx_change {
     fi
   done;
 }
+
+function just_get_color_ramp_flip {
+  local config;config="$(cat ./color_ramp_flip_config.json)";
+  local color_palette; color_palette="$(echo "$config" | jq -r ".color_palette")";
+  local replacement_length; replacement_length="$(echo "$config" | jq -r ".replacements | length")"
+  local query;
+  local asc_query;
+  local desc_query;
+  local color_ramp;
+  local query_result;
+  local agged_csv;
+  local num_of_rows;
+  local half_of_rows;
+  local mid_step_alpha_value;
+  local to_mid_step;
+  local from_mid_step;
+  local rest;
+  for ((idx=0; idx<replacement_length; idx++)); do
+    color_ramp="$(echo "$config" | jq -r ".replacements[$idx].color_ramp")";
+    asc_query="
+    Select
+      a.r as ar
+      , a.g as ag
+      , a.b as ab
+    where
+      a.color_palette == '${color_palette}'
+      and a.color_ramp == '${color_ramp}'
+    order by a.color_ramp_idx asc
+    "
+    asc_query_result="$(cat ./colors.csv | rbql --with-header --query "$asc_query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
+    desc_query="
+    Select
+      a.r as br
+      , a.g as bg
+      , a.b as bb
+    where
+      a.color_palette == '${color_palette}'
+      and a.color_ramp == '${color_ramp}'
+    order by a.color_ramp_idx desc
+    "
+    desc_query_result="$(cat ./colors.csv | rbql --with-header --query "$desc_query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
+    agged_csv="$(paste -d "," <(echo "$asc_query_result") <(echo "$desc_query_result"))";
+    query="
+    Select
+      '{' + f'from=Color({a.ar}, {a.ag}, {a.ab})' + f', to=Color({a.br}, {a.bg}, {a.bb})' + '},' as color_mapping
+    ";
+    query_result="$(echo "$agged_csv" | rbql --with-header --query "$query" --delim ',' --policy quoted_rfc 2> /dev/null | sed -E 's,(^"|"$),,g')";
+    num_of_rows=$(echo -n "$query_result" | wc -l);
+    half_of_rows=$(( (num_of_rows / 2) + (num_of_rows & 1) + 1 ));
+    mid_step_alpha_value="72";
+    to_mid_step="$(echo "$query_result" | head -n "$half_of_rows" | sed -E "s/(.*)\\)(.*?)/\\1, ${mid_step_alpha_value})\\2/g")";
+    rest="$(echo "$query_result" | tail -n +"$(( half_of_rows + 1 ))")";
+    from_mid_step="$(echo "$query_result" | head -n "$half_of_rows" | sed -E "s/.*to=(Color.*?)\\).*/{from=\\1, ${mid_step_alpha_value}), to=\\1)},/")";
+    if [[ "${idx}" == "0" ]]; then
+      echo "$to_mid_step";
+    else
+      echo "$to_mid_step" | tail -n +2;
+    fi
+    echo "$rest";
+    echo "$from_mid_step" | tail -n +2;
+  done;
+}
