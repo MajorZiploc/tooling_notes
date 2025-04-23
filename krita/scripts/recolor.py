c3c3ba3861ab1465888055a625dc9c51792f4c1d
Manyu Lakhotia <lakemonyew@gmail.com>
2025-04-23 13:36:24 -0500

    ^krita recolor; dirty working

--- krita/scripts/recolor.py
+++ krita/scripts/recolor.py
@@ -1,5 +1,5 @@
-from krita import Krita, Document, QImage, QByteArray
-
+from krita import Krita, Document, QImage, QByteArray, QColor
+ 
 def main():
 
     def process_layer(layer):
@@ -15,15 +15,47 @@ def main():
         # img1 = QImage()
         # img1.loadFromData(data)
         # img1.rgbSwapped()
-        img1 = QImage(data, doc.width(), doc.height(), QImage.Format_RGBA8888).rgbSwapped()
-        ptr = img1.bits()
+        # QImage.Format_RGB32
+        # img1 = QImage(data, doc.width(), doc.height(), QImage.Format_RGBA8888).rgbSwapped()
         # print('img1')
         # print(img1)
         # print('img1.bits()')
         # print(img1.bits())
+
+        # address the TODO in this python krita plugin code snippet
+        # NOTE: dont worry about undefined vars and such, this is a snippet from a larger code base
+        # img1 = QImage(data, doc.width(), doc.height(), QImage.Format_RGBA32)
+        # ptr = img1.bits()
+        # # TODO: change all (r,g,b) = (55,22,3) to (r,g,b) = (62, 233, 1)
+        # ptr.setsize(img1.byteCount())
+        # layer.setPixelData(QByteArray(ptr.asstring()), 0, 0, img1.width(), img1.height())
+        # doc.refreshProjection()
+
+        img1 = QImage(data, doc.width(), doc.height(), QImage.Format_RGBA8888)
+        for y in range(img1.height()):
+            for x in range(img1.width()):
+                color = QColor(img1.pixel(x, y))
+                # print('------colors BEGIN')
+                # print('color.red()')
+                # print(color.red())
+                # print('color.green()')
+                # print(color.green())
+                # print('color.blue()')
+                # print(color.blue())
+                # print('------colors END')
+                # red is blue and blue is red - someone messed up the api
+                if color.red() == 3 and color.green() == 22 and color.blue() == 55:
+                    img1.setPixelColor(x, y, QColor(62, 233, 1, color.alpha()))
+                    # print('CHANGE!')
+                # break
+            # break
+
+        ptr = img1.bits()
         ptr.setsize(img1.byteCount())
         layer.setPixelData(QByteArray(ptr.asstring()), 0, 0, img1.width(), img1.height())
         doc.refreshProjection()
+
+
         #
         #
         # width, height = doc.width(), doc.height()
@@ -55,13 +87,18 @@ def main():
             if child.type() == "grouplayer":
                 traverse_layers(child)
 
-    print('hi')
     app = Krita.instance()
     doc = app.activeDocument()
     if doc:
         print(f'Active Document: {doc.fileName()}')
-        root_node = doc.rootNode()
-        traverse_layers(root_node)
+        print('doc.animationLength()')
+        print(doc.animationLength())
+        for time in range(0, doc.animationLength()):
+            doc.setCurrentTime(time)
+            print('doc.currentTime()')
+            print(doc.currentTime())
+            root_node = doc.rootNode()
+            traverse_layers(root_node)
     else:
         print("No active document found.")
 
